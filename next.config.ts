1bcf95ccf0f4a86f6016e36e0c5f6fc94a5fc34e
tranhuy105 <imhuy105@gmail.com>
2025-05-19 03:05:55 +0700

    resolve config warning

--- next.config.ts
+++ next.config.ts
@@ -18,9 +18,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    serverComponentsExternalPackages: ["sharp", "fs", "path"],
-  },
+  serverExternalPackages: ["sharp", "fs", "path"],
 };
 
 export default nextConfig;
